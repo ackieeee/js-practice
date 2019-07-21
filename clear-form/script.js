11cd67ba78ab9b5bddde3b07cf605c14de78bd0a
gba <gba>
2019-07-21 18:34:34 +0900

    関数化した

--- clear-form/script.js
+++ clear-form/script.js
@@ -1,10 +1,13 @@
-(function() {
-  var form = document.getElementsByClassName('form-control');
-  var clearBtn = document.getElementsByClassName('clear-btn');
+(function () {
+  function clearForm() {
+    var form = document.getElementsByClassName('form-control');
+    var clearBtn = document.getElementsByClassName('clear-btn');
 
-  Array.prototype.forEach.call(clearBtn, function(elem, index) {
-    clearBtn.item(index).addEventListener('click', function() {
-      form.item(index).value = "";
+    Array.prototype.forEach.call(clearBtn, function (elem, index) {
+      clearBtn.item(index).addEventListener('click', function () {
+        form.item(index).value = "";
+      })
     })
-  })
+  }
+  clearForm();
 }())
